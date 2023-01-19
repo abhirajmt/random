e9019bed688e0c5d09e192b8843ea28745829ad3
Abhiraj Kumar <abhirajkumar@Abhiraj-Kumar.local>
2023-01-19 14:45:08 +0530

    fix: react->next routing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,22 @@
 import App from 'next/app';
 import '@/styles/globals.css';
 import withReactRouter from '@/hoc/with-react-router';
-import { useLocation } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
   const location = useLocation();
+  const navigate = useNavigate();
   const router = useRouter()
 
   useEffect(() => {
     router.push(location.pathname, undefined, { shallow: false })
   }, [location.pathname]);
+
+  useEffect(() => {
+    navigate(router.asPath);
+  }, [router.asPath]);
 
   return <Component {...pageProps} />
 }
