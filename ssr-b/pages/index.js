9e02d69f5fd55159b25ee86c86fe34fa6bb7fb01
Rohit Garg <rohit.garg@mindtickle.com>
2023-01-23 17:33:39 +0530

    two ssr setup

---
+++ ssr-b/pages/index.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>SSR B</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p className={styles.description}>SSR App B</p>
+      </main>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
