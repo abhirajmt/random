0713e80052ee41eea96e1ec55f5625566d570e00
Rohit Garg <rohit.garg@mindtickle.com>
2023-01-24 13:24:39 +0530

    added CSR A app

---
+++ csr-a/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>This is via main file of csr-a</div>
+    </>
+  );
+}
+
+export default App;
---
+++ csr-a/src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Nav = () => {
+  return <nav>This is via nav file of csr-a</nav>;
+};
+
+export default Nav;
