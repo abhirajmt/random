f31c997235f74eb219bb5fc3eb97dc9ad7e2c740
Rohit Garg <rohit.garg@mindtickle.com>
2023-01-24 16:06:57 +0530

    added log stmt

--- ssr-b/components/nav.js
+++ ssr-b/components/nav.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Link from "next/link";
 
-const Nav = () => (
-  <nav>
-    Nav from SSR B
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-        <Link href="/shop">Shop</Link>
-        <Link href="/p/something">Federated Catch All</Link>
-        <Link href="/checkout">Checkout</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Nav = () => {
+  console.count("ssb-nav");
 
+  return (
+    <nav>
+      Nav from SSR B
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+          <Link href="/shop">Shop</Link>
+          <Link href="/p/something">Federated Catch All</Link>
+          <Link href="/checkout">Checkout</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Nav;
